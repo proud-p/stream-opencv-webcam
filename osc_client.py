cd38f410867240496bee1100485697a2acb17697
proud-p <avika.pulges@hogarth.com>
2025-03-24 17:22:08 +0000

    rename streak to stream flask

---
+++ osc_client.py
@@ -0,0 +1,27 @@
+
+from pythonosc import udp_client
+from time import sleep
+
+def get_message():
+    return "Message from another script!"
+
+ip = "127.0.0.1"    # Localhost
+port = 8001     # Your desired port
+osc_client = udp_client.SimpleUDPClient(ip, port)
+
+def trigger_unreal():
+    """
+    This function sends an OSC message to Unreal Engine
+    """
+    message_address = "/trigger"
+    value = get_message()  # Value to be sent (you can change this as needed 
+    # fetch message dynamically
+
+    # Send the OSC message
+    osc_client.send_message(message_address, value)
+    print(f"Sent OSC message to {ip}:{port} with value:{value}")
+
+
+while True:
+    trigger_unreal()
+    sleep(1)
--- stream_flask.py
+++ stream_flask.py
@@ -8,6 +8,12 @@ def video_feed():
     """Serves video with XYZ coordinates embedded in HTTP headers."""
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
+# @app.route('/video_copy')
+# def video_copy():
+#     """Serves video with XYZ coordinates embedded in HTTP headers."""
+#     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+# #TODO add a route for voice question - should be question, and newly received = True/false
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=False)
     print("Video feed started at http://127.0.0.1:5000/video_feed")
